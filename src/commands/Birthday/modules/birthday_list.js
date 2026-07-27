012734734a88f118721eba432b1899554652ac7f
Sactlord <sactalejandro@gmail.com>
2026-07-27 15:30:16 -0500

    Translate birthday list messages to Spanish

--- src/commands/Birthday/modules/birthday_list.js
+++ src/commands/Birthday/modules/birthday_list.js
@@ -4,6 +4,7 @@ import { deleteBirthday } from '../../../utils/database.js';
 import { logger } from '../../../utils/logger.js';
 
 import { InteractionHelper } from '../../../utils/interactionHelper.js';
+
 export default {
     async execute(interaction, config, client) {
         await InteractionHelper.safeDefer(interaction);
@@ -15,8 +16,9 @@ export default {
         if (sortedBirthdays.length === 0) {
             const embed = new EmbedBuilder()
                 .setColor(0xFF0000)
-                .setTitle('No Birthdays')
-                .setDescription('No birthdays have been set in this server yet.');
+                .setTitle('No hay cumpleaños')
+                .setDescription('Aún no se ha registrado ningún cumpleaños en este servidor.');
+
             return await InteractionHelper.safeEditReply(interaction, {
                 embeds: [embed]
             });
@@ -34,8 +36,9 @@ export default {
                 staleUserIds.push(birthday.userId);
                 continue;
             }
+
             displayIndex++;
-            birthdayList += `${displayIndex}. <@${birthday.userId}> - ${birthday.monthName} ${birthday.day}\n`;
+            birthdayList += `${displayIndex}. <@${birthday.userId}> - ${birthday.day} de ${birthday.monthName}\n`;
         }
 
         if (fetchedMembers && staleUserIds.length > 0) {
@@ -47,25 +50,26 @@ export default {
         if (displayIndex === 0) {
             const embed = new EmbedBuilder()
                 .setColor(0xFF0000)
-                .setTitle('No Birthdays')
-                .setDescription('No birthdays have been set by current server members.');
+                .setTitle('No hay cumpleaños')
+                .setDescription('Ningún miembro actual del servidor ha registrado su cumpleaños.');
+
             return await InteractionHelper.safeEditReply(interaction, {
                 embeds: [embed]
             });
         }
 
-        birthdayList = `**${displayIndex} birthday${displayIndex !== 1 ? 's' : ''} in ${interaction.guild.name}**\n\n` + birthdayList;
+        birthdayList = `**${displayIndex} ${displayIndex !== 1 ? 'cumpleaños registrados' : 'cumpleaños registrado'} en ${interaction.guild.name}**\n\n${birthdayList}`;
 
         const embed = new EmbedBuilder()
             .setColor(0x00FF00)
-            .setTitle('Server Birthdays')
-            .setDescription(`${birthdayList}\n\nTotal: ${displayIndex} birthday${displayIndex !== 1 ? 's' : ''}`);
+            .setTitle('Cumpleaños del servidor')
+            .setDescription(`${birthdayList}\n\nTotal: ${displayIndex} ${displayIndex !== 1 ? 'cumpleaños' : 'cumpleaños'}`);
 
         await InteractionHelper.safeEditReply(interaction, {
             embeds: [embed]
         });
 
-        logger.info('Birthday list retrieved successfully', {
+        logger.info('Lista de cumpleaños obtenida correctamente', {
             userId: interaction.user.id,
             guildId,
             birthdayCount: displayIndex,
